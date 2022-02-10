a7042a56ee2a188998bf72a5f47f82533502655e
Jordan <jardsit@outlook.com>
2022-02-10 15:48:59 +1100

    added project name input

--- makego.ps1
+++ makego.ps1
@@ -5,17 +5,20 @@
 
 md .\cmd  -ErrorAction Continue; Write-Host '.\cmd alread exists'
 New-Item -Path '.\cmd\main.go' -ItemType File -ErrorAction Continue; Write-Host '.\cmd\main.go alread exists'
-Add-Content .\cmd\main.go 'package main
 
-	
+projectName = Read-Host "Please Enter the project name: "
+
+Add-Content .\cmd\main.go 'package main
 
 import (
     "fmt"
 )
-	
+
+var VERSION string = 0.1
+var PROJECTNAME = "'$projectName'"
 
 func main() {
-    fmt.Println("hello world")
+    fmt.Printf("%s %s ", PROJECTNAME, VERSION)
 }
 ';
 md .\internal -ErrorAction Continue; Write-Host '.\internal alread exists'
@@ -24,9 +27,7 @@ md .\configs -ErrorAction Continue; Write-Host '.\configs alread exists'
 md .\docs -ErrorAction Continue; Write-Host '.\docs alread exists'
 
 go run '.\cmd\main.go'
-$project_name = Read-Host -Prompt "Enter your project name: "
 
-
-go mod init $project_name
+go mod init $projectName
 
 go mod tidy
